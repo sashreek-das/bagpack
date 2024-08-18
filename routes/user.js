51f17b80fb976060de397d008ba45f63aa6ea778
Sashreek-PC <sashreek102003@gmail.com>
2024-08-18 20:53:45 +0530

    bug fixes

--- routes/user.js
+++ routes/user.js
@@ -139,26 +139,27 @@ const updateBody = zod.object({
 router.get("/bulk", async (req, res) => {
     const filter = req.query.filter || "";
 
-    const users = await User.find({
-        $or: [{
-            firstName: {
-                "$regex": filter
-            }
-        }, {
-            lastName: {
-                "$regex": filter
-            }
-        }]
-    })
+    let users;
+    if (filter) {
+        users = await User.find({
+            $or: [
+                { firstName: { "$regex": filter, "$options": "i" } },
+                { lastName: { "$regex": filter, "$options": "i" } }
+            ]
+        });
+    } else {
+        users = await User.find({});
+    }
 
     res.json({
-        user: users.map(user => ({
+        users: users.map(user => ({
             username: user.username,
             firstName: user.firstName,
             lastName: user.lastName,
             _id: user._id
         }))
-    })
-})
+    });
+});
+
 
 module.exports = router
